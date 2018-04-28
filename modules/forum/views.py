2b56bc2b8d4f97fb5c522f3a5862c10abc6f6076
Aditya Yanuar <adityanuar@gmail.com>
2018-04-28 14:44:52 +0700

    BA-5: Forum

---
+++ modules/forum/views.py
@@ -0,0 +1,14 @@
+from flask import render_template, Blueprint, jsonify
+module_forum = Blueprint(
+    'module_forum', #name of module
+    __name__,
+    template_folder='templates' # templates folder
+)
+@module_forum.route('/')
+def index():
+    data = [
+        {"id": 1, "title":"Flask vs Golang"},
+        {"id": 2, "title":"Best Python framework out there?"},
+        {"id": 3, "title":"What to do using python in machine learning"}
+    ]
+    return jsonify(data)
