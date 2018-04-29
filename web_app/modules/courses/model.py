84448125caa2ba7a3a201d48f0ea6b48ccd0d5db
Reza Andriyunanto <reza.andriyunanto@redkendi.com>
2018-04-29 09:46:55 +0700

    [BA-17] Update connection to database using SQLAlchemist

--- web_app/modules/courses/model.py
+++ web_app/modules/courses/model.py
@@ -1,5 +1,18 @@
-class Courses():
-    def __init__(self, id, title, url):
-        self.id = id
-        self.title = title
-        self.url = url
+from web_app.modules import db
+from sqlalchemy import Integer, Column, String
+
+
+class Courses(db.Model):
+    __tablename__ = 'course'
+    id = Column(Integer, primary_key= True)
+    title = Column(String)
+    url = Column(String)
+    description = Column(String)
+
+    def to_dict(self):
+        data = {}
+        data['id'] = self.id
+        data['title'] = self.title
+        data['url'] = self.url
+        data['description'] = self.description
+        return data
